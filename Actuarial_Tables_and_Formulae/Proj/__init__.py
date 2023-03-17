96dce9a23fb224704b16a103f1a8ecfcc4cfa3fb
cjpaine <christopher.paine@hotmail.co.uk>
2023-03-17 15:16:21 +0000

    commentary to discount rate(i,t) ; would like to change this function so that t=0 has discount factor of 1

--- Actuarial_Tables_and_Formulae/Proj/__init__.py
+++ Actuarial_Tables_and_Formulae/Proj/__init__.py
@@ -12,7 +12,11 @@ _spaces = []
 # Cells
 
 def discount_rate(i,t):
-
+    '''
+    a 2 parameter function that takes our age of each model point at time t  i.e. age(t)
+    and creates a discount factor such that
+    at t=0 we have a factor of 1 year
+    '''
     df = age(t)
     # Create a new column 'discount_rate' by multiplying t by 0.04
     df['discount_rate'] = ((1+i)**-(t+1))
